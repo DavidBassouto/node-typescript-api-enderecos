5a5bd199d88267d8e4859661ab7f8792ffa9f886
DavidBassouto <davidbassouto@hotmail.com>
2023-06-28 00:20:43 -0300

    feat: listagem retorna array vazio se query nao encontrar resultados; regras e verificacoes para criar UF

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ app.use(express.json());
 
 let conexao: any = null;
 
+//#region SERVICES
 async function abrirConexao() {
   if (conexao == null) {
     console.log("TENTANDO ABRIR CONEXÃO...");
@@ -51,10 +52,107 @@ async function rollback() {
   }
 }
 
+async function varificarExistenciaUF(sigla: string, nome: string) {
+  const listaUFs = [];
+  await abrirConexao();
+  let sql = "SELECT CODIGO_UF, SIGLA, NOME, STATUS FROM TB_UF";
+  sql += ` WHERE NOME='${nome}' AND SIGLA='${sigla}'`;
+
+  console.log("*****************SQL: ", sql);
+
+  const resultado = await conexao.execute(sql);
+  let numeroLinha = 0;
+  let numeroColuna = 0;
+  const quantidadeResultados = resultado.rows.length;
+
+  while (numeroLinha < quantidadeResultados) {
+    const ufVo = {
+      codigoUF: resultado.rows[numeroLinha][numeroColuna++],
+      sigla: resultado.rows[numeroLinha][numeroColuna++],
+      nome: resultado.rows[numeroLinha][numeroColuna++],
+      status: resultado.rows[numeroLinha][numeroColuna++],
+    };
+    listaUFs.push(ufVo);
+    numeroLinha++;
+    numeroColuna = 0;
+  }
+  await fecharConexao();
+
+  return listaUFs[0];
+}
+//#endregion
+
+//#region GET UF
+
 app.get("/uf", function (request, response) {
   return consultarUF(request, response);
 });
 
+async function consultarUF(request: Request, response: Response) {
+  const listaUFs = [];
+  await abrirConexao();
+  let sql = "SELECT CODIGO_UF, SIGLA, NOME, STATUS FROM TB_UF";
+  const queryParams = [];
+  const queryKeys = ["codigoUF", "sigla", "nome", "status"];
+
+  for (let key of queryKeys) {
+    if (request.query[key]) {
+      if (key == "codigoUF") {
+        key = "codigo_uf";
+        queryParams.push(
+          `${key.toUpperCase()}= '${request.query["codigoUF"]}'`,
+        );
+      } else {
+        queryParams.push(
+          `${key.toUpperCase()}= '${request.query[key]}'`,
+        );
+      }
+    }
+  }
+
+  if (queryParams.length > 0) {
+    sql += ` WHERE ${queryParams.join(" AND ")}`;
+  }
+
+  console.log("*****************SQL: ", sql);
+
+  const resultado = await conexao.execute(sql);
+  let numeroLinha = 0;
+  let numeroColuna = 0;
+  const quantidadeResultados = resultado.rows.length;
+
+  while (numeroLinha < quantidadeResultados) {
+    const ufVo = {
+      codigoUF: resultado.rows[numeroLinha][numeroColuna++],
+      sigla: resultado.rows[numeroLinha][numeroColuna++],
+      nome: resultado.rows[numeroLinha][numeroColuna++],
+      status: resultado.rows[numeroLinha][numeroColuna++],
+    };
+    listaUFs.push(ufVo);
+    numeroLinha++;
+    numeroColuna = 0;
+  }
+
+  console.log(resultado.rows);
+  await fecharConexao();
+
+  if (
+    request.query.codigoUF ||
+    request.query.nome ||
+    request.query.sigla
+  ) {
+    if (listaUFs.length == 0) {
+      return response.status(200).json(listaUFs);
+    } else {
+      return response.status(200).json(listaUFs[0]);
+    }
+  }
+
+  response.status(200).json(listaUFs);
+}
+//#endregion
+
+//#region  POST UF
 app.post("/uf", async (request, response) => {
   return await adicionarUF(request, response);
 });
@@ -63,6 +161,59 @@ async function adicionarUF(request: Request, response: Response) {
   try {
     //CAPTURAR OS DADOS QUE VIERAM DA REQUISIÇÃO (FORMATO JSON)
     const ufVo = request.body;
+
+    //VERIFICAR SE SIGLA TEM 2 CARACTERES
+    if (ufVo.sigla.length != 2) {
+      const jsonRetorno = {
+        status: 404,
+        mensagem: "Sigla deve conter 2 caracteres.",
+      };
+      return response.status(404).json(jsonRetorno);
+    }
+
+    //VERIFICAR SE SIGLA POSSUI NUMEROS
+    const regex = /^[A-Za-z\s]+$/;
+
+    if (!regex.test(ufVo.sigla)) {
+      const jsonRetorno = {
+        status: 404,
+        mensagem: "Sigla deve conter apenas letras.",
+      };
+      return response.status(404).json(jsonRetorno);
+    }
+
+    //VERIFICAR SE NOME POSSUI NUMEROS
+
+    if (!regex.test(ufVo.nome)) {
+      const jsonRetorno = {
+        status: 404,
+        mensagem: "Nome deve conter apenas letras.",
+      };
+      return response.status(404).json(jsonRetorno);
+    }
+    
+    //VERIFICAR VALOR DE STATUS
+    if (!(ufVo.status == 1 || ufVo.status == 2)) {
+      const jsonRetorno = {
+        status: 400,
+        mensagem: "Valor inválido para o campo Status.",
+      };
+      return response.status(400).json(jsonRetorno);
+    }
+
+    //VERIFICAR SE EXISTE REGISTRO COM MESMO VALOR NO BANCO
+    const itemDuplicado = await varificarExistenciaUF(
+      ufVo.sigla,
+      ufVo.nome,
+    );
+    if (itemDuplicado) {
+      const jsonRetorno = {
+        status: 400,
+        mensagem: "O item já existe no banco de dados.",
+      };
+      return response.status(400).json(jsonRetorno);
+    }
+
     //ABRIR A CONEXÃO
     await abrirConexao();
     //GERAR UM CÓDIGO POR MEIO DE UMA SEQUENCE
@@ -94,65 +245,7 @@ async function adicionarUF(request: Request, response: Response) {
     await fecharConexao();
   }
 }
-
-async function consultarUF(request: Request, response: Response) {
-  const listaUFs = [];
-  await abrirConexao();
-  let sql = "SELECT CODIGO_UF, SIGLA, NOME, STATUS FROM TB_UF";
-  const queryParams = [];
-  const queryKeys = ["codigoUF", "sigla", "nome", "status"];
-
-  for (let key of queryKeys) {
-    if (request.query[key]) {
-      if (key == "codigoUF") {
-        key = "codigo_uf";
-        queryParams.push(
-          `${key.toUpperCase()}= '${request.query["codigoUF"]}'`,
-        );
-      } else {
-        queryParams.push(
-          `${key.toUpperCase()}= '${request.query[key]}'`,
-        );
-      }
-    }
-  }
-
-  if (queryParams.length > 0) {
-    sql += ` WHERE ${queryParams.join(" AND ")}`;
-  }
-
-  console.log(sql);
-
-  const resultado = await conexao.execute(sql);
-  let numeroLinha = 0;
-  let numeroColuna = 0;
-  const quantidadeResultados = resultado.rows.length;
-
-  while (numeroLinha < quantidadeResultados) {
-    const ufVo = {
-      codigoUF: resultado.rows[numeroLinha][numeroColuna++],
-      sigla: resultado.rows[numeroLinha][numeroColuna++],
-      nome: resultado.rows[numeroLinha][numeroColuna++],
-      status: resultado.rows[numeroLinha][numeroColuna++],
-    };
-    listaUFs.push(ufVo);
-    numeroLinha++;
-    numeroColuna = 0;
-  }
-
-  console.log(resultado.rows);
-  await fecharConexao();
-
-  if (
-    request.query.codigoUF ||
-    request.query.nome ||
-    request.query.sigla
-  ) {
-    return response.status(200).json(listaUFs[0]);
-  }
-
-  response.status(200).json(listaUFs);
-}
+//#endregion POST UF
 
 app.listen(3333, () => {
   console.log("O SERVIDOR FOI INICIADO COM SUCESSO..");
